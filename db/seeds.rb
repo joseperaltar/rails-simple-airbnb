a63b7f6c3422a4cf4abdaa016b4caf14622bd988
Jose Peralta <joseperalta2910@gmail.com>
2024-02-23 16:41:09 -0400

    Excercise solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Flat.destroy_all
+
+Flat.create(name: "Example flat 1", address: "Venezuela", description: "A beatiful flat", price_per_night: 200, number_of_guests: 5)
+Flat.create(name: "Example flat 2", address: "Venezuela", description: "A beatiful flat", price_per_night: 200, number_of_guests: 5)
+Flat.create(name: "Example flat 3", address: "Venezuela", description: "A beatiful flat", price_per_night: 200, number_of_guests: 5)
+Flat.create(name: "Example flat 4", address: "Venezuela", description: "A beatiful flat", price_per_night: 200, number_of_guests: 5)
+Flat.create(name: "Example flat 5", address: "Venezuela", description: "A beatiful flat", price_per_night: 200, number_of_guests: 5)
